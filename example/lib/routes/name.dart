80db4ec97d34394d9925fe189d6bf7709ead1517
lopo <lopo@zju.edu.cn>
2024-04-14 21:03:28 +0800

    feat(example): doc page

--- example/lib/routes/name.dart
+++ example/lib/routes/name.dart
@@ -1,4 +1,10 @@
 abstract class NamedRoute {
   static const boot = '/boot';
+
+  // prefab
+  static const checkbox = '/prefab/checkbox';
+  static const text = '/prefab/text';
+
+  // impl
   static const request = '/request';
 }
--- example/lib/routes/route.dart
+++ example/lib/routes/route.dart
@@ -1,9 +1,17 @@
 import 'package:example/pages/boot.dart';
+import 'package:example/pages/prefab/checkbox.dart';
+import 'package:example/pages/prefab/text.dart';
 import 'package:example/pages/request.dart';
 import 'package:example/routes/name.dart';
 import 'package:get/get.dart';
 
 final List<GetPage> route = [
   GetPage(name: NamedRoute.boot, page: () => const BootPage()),
+
+  // prefab
+  GetPage(name: NamedRoute.checkbox, page: () => const BadCheckBoxDocPage()),
+  GetPage(name: NamedRoute.text, page: () => const BadTextDocPage()),
+
+  // impl
   GetPage(name: NamedRoute.request, page: () => const RequestPage()),
 ];
