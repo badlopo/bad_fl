9239fe884739e3f5c5df663db11f3d4b7f71cba7
lopo <lopo@zju.edu.cn>
2024-04-22 23:26:57 +0800

    docs: fully documented

--- lib/prefab/text_input.dart
+++ lib/prefab/text_input.dart
@@ -4,25 +4,61 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class BadTextInput extends StatefulWidget {
+  /// width of the input field
   final double? width;
+
+  /// height of the input field
   final double height;
+
+  /// initial value of the input field
   final String? initialValue;
+
+  /// placeholder text
   final String? placeholder;
+
+  /// callback when the value changes
   final ValueChanged<String>? onChanged;
+
+  /// callback when the user submits the input (e.g. press enter)
   final ValueSetter<String>? onSubmitted;
 
+  /// type of input (mainly affects keyboard layout on mobile)
   final TextInputType inputType;
+
+  /// action button on mobile keyboard (e.g. done, next, search)
   final TextInputAction textInputAction;
+
+  /// input formatters to restrict input
   final List<TextInputFormatter>? formatters;
+
+  /// text style of the input field
   final TextStyle? style;
+
+  /// text style of the placeholder text
   final TextStyle? placeholderStyle;
 
+  /// space between prefix, input field, and suffix
   final double space;
+
+  /// background color of the input field
   final Color? fill;
+
+  /// border of the input field
   final BoxBorder? border;
+
+  /// border radius of the input field
   final double borderRadius;
 
+  /// widget to display before the input field
+  ///
+  /// Note: there is no constraint on the size of the widget, be careful to its size
   final Widget? prefixWidget;
+
+  /// widget to click to clear the input field
+  ///
+  /// Default to [Icons.close_rounded] with size `16` and color [Colors.grey]
+  ///
+  /// Note: there is no constraint on the size of the widget, be careful to its size if you provide a custom widget
   final Widget clearWidget;
 
   const BadTextInput({
