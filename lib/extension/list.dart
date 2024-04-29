e35f0700ed2aa50840c8a94dbb9d7239a5641a8f
lopo <lopo@zju.edu.cn>
2024-04-29 17:17:39 +0800

    feat: extension on List

---
+++ lib/extension/list.dart
@@ -0,0 +1,15 @@
+extension ListExt<Item> on List<Item> {
+  slotted<Product>({
+    required Product Function(Item item) builder,
+    required Product slot,
+  }) {
+    final result = <Product>[];
+    for (var i = 0; i < length; i++) {
+      result.add(builder(this[i]));
+      if (i < length - 1) {
+        result.add(slot);
+      }
+    }
+    return result;
+  }
+}
