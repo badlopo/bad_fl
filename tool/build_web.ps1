d546ff3d146708318868c2adf25c00379acdc9a7
lopo <lopo@zju.edu.cn>
2024-05-09 10:56:34 +0800

    chore: tool

--- tool/build_web.ps1
+++ tool/build_web.ps1
@@ -1,5 +1,5 @@
 $flutterCommand = "flutter"
 $buildArgs = "build web --base-href /bad_fl/ --release"
 
-Set-Location .\docs\
+Set-Location ..\example\
 Invoke-Expression -Command "$flutterCommand $buildArgs"
