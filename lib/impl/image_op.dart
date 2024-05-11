2f04aaf9054459500028401b8d55e622c5f3027a
lopo <lopo@zju.edu.cn>
2024-05-11 14:04:11 +0800

    feat: expose result

--- lib/impl/image_op.dart
+++ lib/impl/image_op.dart
@@ -3,11 +3,11 @@ import 'dart:typed_data';
 import 'package:image_gallery_saver/image_gallery_saver.dart';
 import 'package:image_picker/image_picker.dart';
 
-class PickResult {
+class PickedItem {
   final String filename;
   final String path;
 
-  const PickResult({
+  const PickedItem({
     required this.filename,
     required this.path,
   });
@@ -15,30 +15,49 @@ class PickResult {
   Map<String, String> toJson() => {'filename': filename, 'path': path};
 }
 
+class SaveResult {
+  final bool ok;
+  final String? filePath;
+  final String? errorMessage;
+
+  SaveResult.fromJson(Map<String, dynamic> json)
+      : ok = json['isSuccess'],
+        filePath = json['filePath'],
+        errorMessage = json['errorMessage'];
+
+  Map<String, dynamic> toJson() {
+    return {
+      'ok': ok,
+      'filePath': filePath,
+      'errorMessage': errorMessage,
+    };
+  }
+}
+
 /// image operation implementation
 abstract class ImageOPImpl {
   static final ImagePicker _picker = ImagePicker();
 
   /// internal method to pick image
-  static Future<PickResult?> _pick(ImageSource from) async {
+  static Future<PickedItem?> _pick(ImageSource from) async {
     final XFile? xFile = await _picker.pickImage(source: from);
     if (xFile == null) return null;
-    return PickResult(filename: xFile.name, path: xFile.path);
+    return PickedItem(filename: xFile.name, path: xFile.path);
   }
 
   /// pick image from gallery
-  static Future<PickResult?> pickGallery() => _pick(ImageSource.gallery);
+  static Future<PickedItem?> pickGallery() => _pick(ImageSource.gallery);
 
   /// pick image from camera
-  static Future<PickResult?> pickCamera() => _pick(ImageSource.camera);
+  static Future<PickedItem?> pickCamera() => _pick(ImageSource.camera);
 
   /// save image to gallery
-  static Future<bool> saveToGallery(Uint8List buffer) async {
+  static Future<SaveResult> saveToGallery(Uint8List buffer) async {
     try {
-      await ImageGallerySaver.saveImage(buffer);
-      return true;
-    } catch (_) {
-      return false;
+      final r = await ImageGallerySaver.saveImage(buffer);
+      return SaveResult.fromJson(Map<String, dynamic>.from(r));
+    } catch (err) {
+      return SaveResult.fromJson({'isSuccess': false, 'errorMessage': '$err'});
     }
   }
 }
