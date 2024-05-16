58d2b35ae9ce5b71771bb1cbb9b7ea8ef7408c94
lopo <lopo@zju.edu.cn>
2024-05-17 00:02:19 +0800

    feat: Improve the usage of 'iconBuilder'

--- lib/prefab/checkbox.dart
+++ lib/prefab/checkbox.dart
@@ -1,20 +1,19 @@
 import 'package:bad_fl/wrapper/clickable.dart';
 import 'package:flutter/material.dart';
 
-typedef IconBuilder = Widget Function(bool checked);
-
-/// Note: this is a controlled checkbox, you need to manage the state of the checkbox yourself.
 class BadCheckBox extends StatelessWidget {
   /// size of the checkbox
   final double size;
 
-  /// icon to be displayed inside the checkbox, can't be used with [iconBuilder]
+  /// icon to be displayed inside the checkbox when checked, can't be used with [iconBuilder]
   final Widget? icon;
 
-  /// builder for the icon to be displayed inside the checkbox, can't be used with [icon]
-  final IconBuilder? iconBuilder;
+  /// builder for the icon to be displayed inside the checkbox, return `null` to hide the icon, can't be used with [icon]
+  final Widget? Function(bool checked)? iconBuilder;
 
-  /// size of the icon inside the checkbox, default to [size]
+  /// size of the icon inside the checkbox
+  ///
+  /// Default to [size]
   final double iconSize;
 
   /// border of the checkbox
@@ -23,14 +22,22 @@ class BadCheckBox extends StatelessWidget {
   /// whether the checkbox is rounded
   final bool rounded;
 
-  /// color of the checkbox when checked, default to null
-  final Color? checkedColor;
+  /// color of the checkbox when unchecked
+  final Color? fill;
 
-  /// whether the checkbox is checked initially, default to false
+  /// color of the checkbox when checked
+  ///
+  /// Default to [fill]
+  final Color? fillChecked;
+
+  /// whether the checkbox is checked
   final bool checked;
 
   /// callback when the checkbox is tapped
   final VoidCallback onTap;
+
+  /// whether to use [icon] or [iconBuilder]
+  final bool _useBuilder;
 
   const BadCheckBox.icon({
     super.key,
@@ -39,11 +46,14 @@ class BadCheckBox extends StatelessWidget {
     double? iconSize,
     this.border,
     this.rounded = true,
-    this.checkedColor,
+    this.fill,
+    Color? fillChecked,
     required this.checked,
     required this.onTap,
   })  : iconSize = iconSize ?? size,
         iconBuilder = null,
+        fillChecked = fillChecked ?? fill,
+        _useBuilder = false,
         assert(icon != null, 'icon must be provided');
 
   const BadCheckBox.iconBuilder({
@@ -53,15 +63,15 @@ class BadCheckBox extends StatelessWidget {
     double? iconSize,
     this.border,
     this.rounded = true,
-    this.checkedColor,
+    this.fill,
+    Color? fillChecked,
     required this.checked,
     required this.onTap,
   })  : iconSize = iconSize ?? size,
         icon = null,
+        fillChecked = fillChecked ?? fill,
+        _useBuilder = true,
         assert(iconBuilder != null, 'iconBuilder must be provided');
-
-  /// icon to be displayed inside the checkbox
-  Widget get _innerIcon => icon ?? iconBuilder!(checked);
 
   @override
   Widget build(BuildContext context) {
@@ -76,19 +86,20 @@ class BadCheckBox extends StatelessWidget {
             height: size,
             clipBehavior: Clip.antiAlias,
             decoration: BoxDecoration(
-              color: checked ? checkedColor : null,
+              color: checked ? fillChecked : fill,
               border: border,
               borderRadius: rounded ? BorderRadius.circular(size / 2) : null,
             ),
-            child: checked
-                ? ConstrainedBox(
-                    constraints: BoxConstraints(
-                      maxWidth: iconSize,
-                      maxHeight: iconSize,
-                    ),
-                    child: Center(child: _innerIcon),
-                  )
-                : null,
+            alignment: Alignment.center,
+            child: SizedBox(
+              width: iconSize,
+              height: iconSize,
+              child: _useBuilder
+                  ? iconBuilder!(checked)
+                  : checked
+                      ? icon
+                      : null,
+            ),
           ),
         ),
       ),
