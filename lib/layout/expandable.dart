c431da555b5027d02038f1e33c288b61870d64aa
lopo <lopo@zju.edu.cn>
2024-05-23 16:58:22 +0800

    refactor: drop getx

--- lib/layout/expandable.dart
+++ lib/layout/expandable.dart
@@ -1,95 +1,117 @@
 import 'package:bad_fl/wrapper/clickable.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
-class BadExpandable extends StatelessWidget {
-  /// whether the panel is open or not.
-  final RxBool _open = true.obs;
+class BadExpandable extends StatefulWidget {
+  /// whether the panel is open initially or not
+  ///
+  /// Default to `true`
+  final bool initialOpen;
 
-  /// height of the header. default to `28.0`.
+  /// height of the header
+  ///
+  /// Default to `28`
   final double headerHeight;
 
-  /// gap between the header and the child. default to `20.0`.
+  /// gap between the header and the child
+  ///
+  /// Default to `0`
   final double gap;
 
   /// title of the panel.
   final Widget title;
 
-  /// icon when the panel is empty, required when [child] is `null`.
+  /// icon to be displayed when the panel is empty.
+  ///
+  /// Note: required when [child] is `null`.
   final Widget? emptyIcon;
 
-  /// icon when the panel is open.
+  /// icon to be displayed when the panel is open.
+  ///
+  /// Default to `Icon(Icons.arrow_drop_up, size: 24)`
   final Widget openedIcon;
 
-  /// icon when the panel is closed.
+  /// icon to be displayed when the panel is closed.
+  ///
+  /// Default to `Icon(Icons.arrow_drop_down, size: 24)`
   final Widget closedIcon;
 
   /// child widget to be displayed when the panel is open.
+  ///
+  /// Note: if [child] is `null`, this widget will be treated as an empty panel.
   final Widget? child;
 
-  BadExpandable({
+  const BadExpandable({
     super.key,
-    this.headerHeight = 28.0,
-    this.gap = 20.0,
-    required this.title,
-    this.emptyIcon,
-    this.openedIcon = const Icon(Icons.arrow_drop_up, size: 24),
-    this.closedIcon = const Icon(Icons.arrow_drop_down, size: 24),
-    this.child,
-  }) : assert(child != null || emptyIcon != null);
-
-  BadExpandable.none({
-    super.key,
-    this.headerHeight = 28.0,
-    this.gap = 20.0,
-    required this.title,
-    required this.emptyIcon,
-    this.openedIcon = const Icon(Icons.arrow_drop_up, size: 24),
-    this.closedIcon = const Icon(Icons.arrow_drop_down, size: 24),
-  })  : child = null,
-        assert(emptyIcon != null);
-
-  BadExpandable.some({
-    super.key,
-    this.headerHeight = 28.0,
-    this.gap = 20.0,
+    this.initialOpen = true,
+    this.headerHeight = 28,
+    this.gap = 0,
     required this.title,
     this.openedIcon = const Icon(Icons.arrow_drop_up, size: 24),
     this.closedIcon = const Icon(Icons.arrow_drop_down, size: 24),
     required this.child,
-  })  : emptyIcon = null,
-        assert(child != null);
+  })  : assert(child != null),
+        emptyIcon = null;
+
+  const BadExpandable.empty({
+    super.key,
+    this.initialOpen = true,
+    this.headerHeight = 28,
+    this.gap = 0,
+    required this.title,
+    this.openedIcon = const Icon(Icons.arrow_drop_up, size: 24),
+    this.closedIcon = const Icon(Icons.arrow_drop_down, size: 24),
+    required this.emptyIcon,
+  })  : assert(emptyIcon != null),
+        child = null;
+
+  @override
+  State<BadExpandable> createState() => _BadExpandableState();
+}
+
+class _BadExpandableState extends State<BadExpandable> {
+  bool _open = false;
+
+  void toggle() {
+    setState(() {
+      _open = !_open;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _open = widget.initialOpen;
+  }
 
   @override
   Widget build(BuildContext context) {
     final header = SizedBox(
-      height: headerHeight,
+      height: widget.headerHeight,
       child: Row(
         children: [
-          Expanded(child: title),
-          if (child == null)
-            emptyIcon!
+          Expanded(child: widget.title),
+          if (widget.child == null)
+            widget.emptyIcon!
           else
-            Obx(() => _open.isTrue ? openedIcon : closedIcon),
+            _open ? widget.openedIcon : widget.closedIcon,
         ],
       ),
     );
 
-    if (child == null) return header;
+    if (widget.child == null) return header;
 
     return AnimatedSize(
       duration: const Duration(milliseconds: 300),
       alignment: Alignment.topCenter,
-      child: Obx(
-        () => Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            BadClickable(onClick: _open.toggle, child: header),
-            if (_open.isTrue) SizedBox(height: gap),
-            if (_open.isTrue) child!,
-          ],
-        ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          BadClickable(onClick: toggle, child: header),
+          if (_open && widget.gap > 0) SizedBox(height: widget.gap),
+          if (_open) widget.child!,
+        ],
       ),
     );
   }
