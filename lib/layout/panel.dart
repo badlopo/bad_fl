06d62f0faeb61ebe4a2fe2a4794c2fc1e7bbd927
lopo <lopo@zju.edu.cn>
2024-05-29 10:06:32 +0800

    refactor: ..

--- lib/layout/panel.dart
+++ lib/layout/panel.dart
@@ -45,31 +45,35 @@ class BadPanelItem extends StatelessWidget {
 }
 
 class BadPanelOptions {
-  /// title of panel, default to `null`
-  final Widget? title;
+  /// background color of panel
+  ///
+  /// Default to `Colors.white`
+  final Color fill;
 
-  /// color of panel, default to [Colors.white]
-  final Color color;
+  /// border radius of panel
+  ///
+  /// Default to `8`
+  final double borderRadius;
 
-  /// border-radius of panel, default to `8`
-  final double radius;
+  /// divider between items
+  ///
+  /// Default to `Divider(height: 0.5, thickness: 0.5, color: Colors.grey)`
+  final Widget? divider;
 
-  /// thickness of divider, default to `0.5`
-  final double dividerThickness;
-
-  /// color of divider between items, default to [Colors.grey]
-  final Color dividerColor;
-
-  /// height of panel item, default to `54`
+  /// height of panel item
+  ///
+  /// Default to `54`
   final double itemHeight;
 
   const BadPanelOptions({
-    this.title,
-    this.color = Colors.white,
-    this.radius = 8,
-    this.dividerThickness = 0.5,
-    this.dividerColor = Colors.grey,
+    this.fill = Colors.white,
+    this.borderRadius = 8,
     this.itemHeight = 54,
+    this.divider = const Divider(
+      height: 0.5,
+      thickness: 0.5,
+      color: Colors.grey,
+    ),
   });
 }
 
@@ -90,47 +94,42 @@ class _BadPanelScope extends InheritedWidget {
 
 class BadPanel extends StatelessWidget {
   final BadPanelOptions options;
+
+  /// title of panel
+  final Widget? title;
+
+  /// items in panel
   final List<BadPanelItem> items;
 
   const BadPanel({
     super.key,
     this.options = const BadPanelOptions(),
+    this.title,
     required this.items,
   }) : assert(items.length > 0, 'require at least 1 item in panel');
 
   @override
   Widget build(BuildContext context) {
+    List<Widget> children = options.divider == null
+        ? items
+        : items.slotted(builder: (item) => item, slot: options.divider!);
+
     Widget panel = Container(
       padding: const EdgeInsets.symmetric(horizontal: 16),
       decoration: BoxDecoration(
-        color: options.color,
-        borderRadius: BorderRadius.circular(options.radius),
+        color: options.fill,
+        borderRadius: BorderRadius.circular(options.borderRadius),
       ),
-      child: Column(
-        children: items.slotted(
-          builder: (item) => item,
-          slot: Divider(
-            height: options.dividerThickness,
-            thickness: options.dividerThickness,
-            color: options.dividerColor,
-          ),
-        ),
-      ),
+      child: Column(children: children),
     );
 
-    if (options.title != null) {
+    if (title != null) {
       panel = Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          options.title!,
-          panel,
-        ],
+        children: [title!, panel],
       );
     }
 
-    return _BadPanelScope(
-      itemHeight: options.itemHeight,
-      child: panel,
-    );
+    return _BadPanelScope(itemHeight: options.itemHeight, child: panel);
   }
 }
