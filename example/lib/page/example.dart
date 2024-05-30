d6d619edf97734fd31bbd085eceb3c4cd268df25
lopo <lopo@zju.edu.cn>
2024-05-30 23:13:08 +0800

    chore: example

--- example/lib/page/example.dart
+++ example/lib/page/example.dart
@@ -1,64 +1,132 @@
+import 'dart:js_interop';
+import 'dart:typed_data';
+import 'dart:ui';
+
 import 'package:bad_fl/bad_fl.dart';
+import 'package:bad_fl/wrapper/snapshot.dart';
 import 'package:flutter/material.dart';
 
-class Example extends StatelessWidget {
+// class Example extends StatelessWidget {
+//   const Example({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.grey[200],
+//       body: ListView(
+//         padding: const EdgeInsets.all(12),
+//         children: [
+//           BadPanel(
+//             title: const Padding(
+//               padding: EdgeInsets.only(bottom: 8),
+//               child: Text('Profile'),
+//             ),
+//             items: const [
+//               BadPanelItem(
+//                 label: Text('Avatar'),
+//                 suffix: CircleAvatar(
+//                   radius: 18,
+//                   backgroundImage: NetworkImage('https://picsum.photos/200'),
+//                 ),
+//               ),
+//               BadPanelItem(
+//                 label: Text('Nickname'),
+//                 body: Text('nickname', textAlign: TextAlign.end),
+//                 suffix: Icon(Icons.edit_note),
+//               ),
+//               BadPanelItem(
+//                 label: Text('HomePage'),
+//                 body: Text('https://example.com', textAlign: TextAlign.end),
+//               ),
+//               BadPanelItem(
+//                 label: Text('About'),
+//                 body: Text(
+//                   'lorem ipsum dolor sit amet, consectetur adipiscing elit',
+//                   maxLines: 2,
+//                   overflow: TextOverflow.ellipsis,
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+@JS('saveImage')
+external JSVoid saveImage(JSUint8Array bytes);
+
+class Example extends StatefulWidget {
   const Example({super.key});
+
+  @override
+  State<Example> createState() => _ExampleState();
+}
+
+class _ExampleState extends State<Example> {
+  final controller = BadSnapshotController();
+
+  void takeSnapshot() async {
+    final image = controller.capture();
+    final byteData = await image.toByteData(format: ImageByteFormat.png);
+    final bytes = byteData!.buffer.asUint8List();
+    saveImage(bytes.toJS);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Example'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.camera_alt),
+            onPressed: takeSnapshot,
+          ),
+        ],
+      ),
       backgroundColor: Colors.grey[200],
       body: ListView(
-        padding: const EdgeInsets.all(12),
+        padding: const EdgeInsets.all(16),
         children: [
-          BadPanel(
-            title: const Padding(
-              padding: EdgeInsets.only(bottom: 8),
-              child: Text('Profile'),
+          BadSnapshot(
+            controller: controller,
+            child: BadPanel(
+              title: const Padding(
+                padding: EdgeInsets.only(bottom: 8),
+                child: Text('Profile'),
+              ),
+              items: const [
+                BadPanelItem(
+                  label: Text('Avatar'),
+                  suffix: CircleAvatar(
+                    radius: 18,
+                    backgroundImage: NetworkImage('https://picsum.photos/200'),
+                  ),
+                ),
+                BadPanelItem(
+                  label: Text('Nickname'),
+                  body: Text('nickname', textAlign: TextAlign.end),
+                  suffix: Icon(Icons.edit_note),
+                ),
+                BadPanelItem(
+                  label: Text('HomePage'),
+                  body: Text('https://example.com', textAlign: TextAlign.end),
+                ),
+                BadPanelItem(
+                  label: Text('About'),
+                  body: Text(
+                    'lorem ipsum dolor sit amet, consectetur adipiscing elit',
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+              ],
             ),
-            items: const [
-              BadPanelItem(
-                label: Text('Avatar'),
-                suffix: CircleAvatar(
-                  radius: 18,
-                  backgroundImage: NetworkImage('https://picsum.photos/200'),
-                ),
-              ),
-              BadPanelItem(
-                label: Text('Nickname'),
-                body: Text('nickname', textAlign: TextAlign.end),
-                suffix: Icon(Icons.edit_note),
-              ),
-              BadPanelItem(
-                label: Text('HomePage'),
-                body: Text('https://example.com', textAlign: TextAlign.end),
-              ),
-              BadPanelItem(
-                label: Text('About'),
-                body: Text(
-                  'lorem ipsum dolor sit amet, consectetur adipiscing elit',
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                ),
-              ),
-            ],
           ),
         ],
       ),
     );
   }
 }
-
-// class Example extends StatefulWidget {
-//   const Example({super.key});
-//
-//   @override
-//   State<Example> createState() => _ExampleState();
-// }
-//
-// class _ExampleState extends State<Example> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold();
-//   }
-// }
