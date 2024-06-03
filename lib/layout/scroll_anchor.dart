1f7c1cdab480bd749359e31faa831839477c6ed4
lopo <lopo@zju.edu.cn>
2024-06-03 14:00:54 +0800

    feat: BadScrollAnchor -- observer on anchor's show/hide

--- lib/layout/scroll_anchor.dart
+++ lib/layout/scroll_anchor.dart
@@ -3,6 +3,17 @@ import 'dart:async';
 import 'package:bad_fl/core.dart';
 import 'package:flutter/material.dart';
 
+enum AnchorStatus {
+  /// the delta between the current position and the previous position is positive
+  hide,
+
+  /// the delta between the current position and the previous position is negative
+  show
+}
+
+typedef BadScrollObserver<AsKey> = void Function(
+    AsKey key, AnchorStatus status);
+
 class BadScrollAnchorController<AsKey extends Object> {
   /// find the `BadScrollAnchorController` of the nearest scope
   ///
@@ -14,24 +25,19 @@ class BadScrollAnchorController<AsKey extends Object> {
         ?.controller;
   }
 
-  /// the scrollDirection of the inner `ScrollView`
-  ///
-  /// Default to `Axis.vertical`
-  final Axis scrollDirection;
-
-  /// the `ScrollController` of the inner `ScrollView`
-  ScrollController? _scrollController;
-
-  void _attach(ScrollController scrollController) {
-    if (_scrollController != null) {
-      throw StateError('The instance is already attached');
+  void _ensureReady() {
+    if (_scrollController == null) {
+      throw StateError('The instance is not attached');
     }
-    _scrollController = scrollController;
+    if (!_scrollController!.hasClients) {
+      throw StateError('The "controller" of "builder" is not attached');
+    }
+    if (_containerKey.currentContext == null) {
+      throw StateError('The "key" of "builder" is not attached');
+    }
   }
 
-  void _detach() {
-    _scrollController = null;
-  }
+  // region anchor management
 
   /// the `GlobalKey` for the inner `ScrollView`
   final GlobalKey _containerKey = GlobalKey(debugLabel: 'container');
@@ -60,14 +66,30 @@ class BadScrollAnchorController<AsKey extends Object> {
     }
 
     _valueToKey[asKey] = key;
+    BadFl.log(module: 'BadScrollAnchor', message: 'add anchor <$asKey>');
   }
 
   /// remove a anchor point
   void _removeAnchor(AsKey asKey) {
     _valueToKey.remove(asKey);
+    BadFl.log(module: 'BadScrollAnchor', message: 'remove anchor <$asKey>');
   }
 
-  double _getOriginPosition() {
+  // endregion
+
+  // region jump/animate to anchor
+  /// try to get the position of the inner `ScrollView` in global coordinates
+  /// return `null` if the widget is not mounted
+  double? _getOriginPositionInGlobal() {
+    // guard -- ensure the widget is mounted so that we can get the RenderObject
+    if (!_containerMounted) {
+      BadFl.log(
+        module: 'BadScrollAnchor',
+        message: 'the container widget is not mounted',
+      );
+      return null;
+    }
+
     final ref = _containerKey.currentContext!.findRenderObject() as RenderBox;
     final tl = ref.localToGlobal(Offset.zero);
 
@@ -79,7 +101,7 @@ class BadScrollAnchorController<AsKey extends Object> {
     }
   }
 
-  double _getAnchorPosition(GlobalKey anchorKey) {
+  double _getAnchorPositionInGlobal(GlobalKey anchorKey) {
     final ref = anchorKey.currentContext!.findRenderObject() as RenderBox;
     final tl = ref.localToGlobal(Offset.zero);
 
@@ -97,16 +119,8 @@ class BadScrollAnchorController<AsKey extends Object> {
   }
 
   /// get the target position related to the key with an optional offset
-  double? getAnchorPos(AsKey key, [double offset = 0]) {
-    if (_scrollController == null) {
-      throw StateError('The instance is not attached');
-    }
-    if (!_scrollController!.hasClients) {
-      throw StateError('The "controller" of "builder" is not attached');
-    }
-    if (_containerKey.currentContext == null) {
-      throw StateError('The "key" of "builder" is not attached');
-    }
+  double? _getAnchorPosInScrollable(AsKey key, [double offset = 0]) {
+    _ensureReady();
 
     final anchorKey = _valueToKey[key];
     if (anchorKey == null) {
@@ -116,8 +130,10 @@ class BadScrollAnchorController<AsKey extends Object> {
       );
       return null;
     } else {
-      final originPosGlobal = _getOriginPosition();
-      final anchorPosGlobal = _getAnchorPosition(anchorKey);
+      final originPosGlobal = _getOriginPositionInGlobal();
+      if (originPosGlobal == null) return null;
+
+      final anchorPosGlobal = _getAnchorPositionInGlobal(anchorKey);
       final scrolledDis = _scrollController!.offset;
 
       double anchorPosInside = anchorPosGlobal + scrolledDis - originPosGlobal;
@@ -129,7 +145,7 @@ class BadScrollAnchorController<AsKey extends Object> {
 
   /// jump to the anchor point related to the key with an optional offset
   void jumpToAnchor(AsKey key, [double offset = 0]) {
-    double? targetPos = getAnchorPos(key, offset);
+    double? targetPos = _getAnchorPosInScrollable(key, offset);
     if (targetPos != null) _scrollController!.jumpTo(targetPos);
   }
 
@@ -140,12 +156,131 @@ class BadScrollAnchorController<AsKey extends Object> {
     required Curve curve,
     double offset = 0,
   }) {
-    double? targetPos = getAnchorPos(key, offset);
+    double? targetPos = _getAnchorPosInScrollable(key, offset);
     if (targetPos != null) {
       return _scrollController!
           .animateTo(targetPos, duration: duration, curve: curve);
     }
   }
+
+  // endregion
+
+  // region observer management
+  /// a mapping of the position of the anchor points to the user-supplied values (used as key) stored in form of `List<(position, key)>`
+  ///
+  /// Note: this is more a snapshot of the position of the anchor points.
+  /// this will be updated when:
+  /// - a anchor point is added/removed
+  /// - the [updateAnchorPos] is called
+  List<(double, AsKey)> _posToValue = [];
+
+  /// update the position of the anchor points in the scope so that the observers can be notified accurately
+  void updateAnchorPos() {
+    _ensureReady();
+
+    final originPosGlobal = _getOriginPositionInGlobal();
+    if (originPosGlobal == null) return;
+
+    final scrollOffset = _scrollController!.offset;
+
+    _posToValue = _valueToKey.entries.map((entry) {
+      final posGlobal = _getAnchorPositionInGlobal(entry.value);
+      double pos = posGlobal + scrollOffset - originPosGlobal;
+      return (pos, entry.key);
+    }).toList()
+      ..sort((a, b) => a.$1.compareTo(b.$1));
+
+    BadFl.log(module: 'BadScrollAnchor', message: 'anchor positions updated');
+  }
+
+  double _lastOffset = 0;
+
+  /// determine the event of the delta between the current position and the previous position
+  (AsKey, AnchorStatus)? _frameEvent() {
+    final currentOffset = _scrollController!.offset;
+
+    if (currentOffset > _lastOffset) {
+      // offset increase -- AnchorStatus.hide
+      for (final (pos, key) in _posToValue) {
+        if (currentOffset > pos && _lastOffset <= pos) {
+          return (key, AnchorStatus.hide);
+        }
+      }
+    } else if (currentOffset < _lastOffset) {
+      // offset decrease -- AnchorStatus.show
+      for (final (pos, key) in _posToValue.reversed) {
+        if (currentOffset < pos && _lastOffset >= pos) {
+          return (key, AnchorStatus.show);
+        }
+      }
+    }
+
+    return null;
+  }
+
+  void _scrollListener() {
+    final currentOffset = _scrollController!.offset;
+
+    final event = _frameEvent();
+    if (event != null) {
+      final (key, status) = event;
+      for (final observer in _observers) {
+        observer(key, status);
+      }
+    }
+
+    _lastOffset = currentOffset;
+  }
+
+  /// a set of observers that will be notified when an anchor passes by
+  final Set<BadScrollObserver<AsKey>> _observers = {};
+
+  void addObserver(BadScrollObserver<AsKey> observer) {
+    _observers.add(observer);
+  }
+
+  void removeObserver(BadScrollObserver<AsKey> observer) {
+    _observers.remove(observer);
+  }
+
+  // endregion
+
+  // region lifecycle
+  /// whether the 'BadScrollAnchorScope' is mounted
+  bool _containerMounted = false;
+
+  void _attach(ScrollController scrollController) {
+    if (_scrollController != null) {
+      throw StateError('The instance is already attached');
+    }
+    _scrollController = scrollController;
+    scrollController.addListener(_scrollListener);
+
+    BadFl.log(
+      module: 'BadScrollAnchorController',
+      message: 'attached to the "BadScrollAnchorScope"',
+    );
+  }
+
+  void _detach() {
+    _scrollController!.removeListener(_scrollListener);
+    _scrollController = null;
+
+    BadFl.log(
+      module: 'BadScrollAnchorController',
+      message: 'detached from the "BadScrollAnchorScope"',
+    );
+  }
+
+  // endregion
+
+  /// the `ScrollController` of the inner `ScrollView`
+  ScrollController? _scrollController;
+
+  /// the scrollDirection of the inner `ScrollView`
+  ///
+  /// Default to `Axis.vertical`
+  final Axis scrollDirection;
 
   BadScrollAnchorController({this.scrollDirection = Axis.vertical});
 }
@@ -186,12 +321,19 @@ class _BadScrollAnchorState<AsKey extends Object>
 
     // add the anchor point when the widget is initialized
     controller._addAnchor(widget.asKey, _anchorKey);
+
+    // update the position of the anchor points in the scope after the layout is built
+    WidgetsBinding.instance
+        .addPostFrameCallback((_) => controller.updateAnchorPos());
   }
 
   @override
   void dispose() {
     // remove the anchor point when the widget is disposed
     controller._removeAnchor(widget.asKey);
+
+    // update the position of the anchor points in the scope
+    controller.updateAnchorPos();
     super.dispose();
   }
 
@@ -250,6 +392,24 @@ class _BadScrollAnchorScopeState<AsKey extends Object>
     super.initState();
     sc = widget.scrollController ?? ScrollController();
     widget.controller._attach(sc);
+
+    // mark the container widget as mounted after the layout is built
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      widget.controller._containerMounted = true;
+    });
+  }
+
+  @override
+  void activate() {
+    widget.controller._containerMounted = true;
+    super.activate();
+  }
+
+  @override
+  void deactivate() {
+    // we need to mark the container widget as unmounted here to make sure it before children widgets' 'dispose' method
+    widget.controller._containerMounted = false;
+    super.deactivate();
   }
 
   @override
