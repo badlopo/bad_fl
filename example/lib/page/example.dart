e28ee2b9ceb0c5e0653c2d39b24bac9818460bc7
lopo <lopo@zju.edu.cn>
2024-06-03 15:17:49 +0800

    chore: example -- BadScrollAnchorScope

--- example/lib/page/example.dart
+++ example/lib/page/example.dart
@@ -1,5 +1,6 @@
 import 'package:bad_fl/bad_fl.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 // class Example extends StatelessWidget {
 //   const Example({super.key});
@@ -57,35 +58,118 @@ class Example extends StatefulWidget {
 }
 
 class _ExampleState extends State<Example> {
-  final controller = ScrollController();
+  final RxInt ptr = 0.obs;
+  final sc = ScrollController();
+  final sac = BadScrollAnchorController<int>();
+
+  @override
+  void initState() {
+    super.initState();
+
+    sac.addObserver((key, status) {
+      if (status == AnchorStatus.show) {
+        ptr.value = key;
+      } else {
+        ptr.value = key + 1;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Example')),
-      floatingActionButton: Column(
-        mainAxisSize: MainAxisSize.min,
+      appBar: AppBar(title: Obx(() => Text('top anchor: $ptr'))),
+      body: Column(
         children: [
-          BadBackToTop(
-            scrollController: controller,
-            child: const Text('jump to top'),
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Row(
+              children: [
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: () => sac.jumpToAnchor(2),
+                    child: const Text('jump to 2'),
+                  ),
+                ),
+                const SizedBox(width: 16),
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: () => sac.animateToAnchor(4),
+                    child: const Text('animate to 4'),
+                  ),
+                ),
+              ],
+            ),
           ),
-          BadBackToTop.animated(
-            scrollController: controller,
-            child: const Text('animate to top'),
+          Expanded(
+            child: BadScrollAnchorScope(
+              controller: sac,
+              scrollController: sc,
+              padding: const EdgeInsets.all(16),
+              children: [
+                BadScrollAnchor(
+                  asKey: 0,
+                  child: Container(
+                    width: double.infinity,
+                    height: 300,
+                    color: Colors.blue,
+                    child: const Text('anchor 0'),
+                  ),
+                ),
+                BadScrollAnchor(
+                  asKey: 1,
+                  child: Container(
+                      width: double.infinity,
+                      height: 400,
+                      color: Colors.grey,
+                      padding: const EdgeInsets.all(50),
+                      child: Column(
+                        children: [
+                          const Text('anchor 1'),
+                          const SizedBox(height: 50),
+                          BadScrollAnchor(
+                            asKey: 2,
+                            child: Container(
+                              width: double.infinity,
+                              height: 100,
+                              color: Colors.cyan,
+                              child: const Text(
+                                  'anchor 2\nnested anchor works too!'),
+                            ),
+                          ),
+                        ],
+                      )),
+                ),
+                BadScrollAnchor(
+                  asKey: 3,
+                  child: Container(
+                    width: double.infinity,
+                    height: 200,
+                    color: Colors.green,
+                    child: const Text('anchor 3'),
+                  ),
+                ),
+                BadScrollAnchor(
+                  asKey: 4,
+                  child: Container(
+                    width: double.infinity,
+                    height: 500,
+                    color: Colors.amber,
+                    child: const Text('anchor 4'),
+                  ),
+                ),
+                BadScrollAnchor(
+                  asKey: 5,
+                  child: Container(
+                    width: double.infinity,
+                    height: 700,
+                    color: Colors.teal,
+                    child: const Text('anchor 5'),
+                  ),
+                ),
+              ],
+            ),
           ),
-        ],
-      ),
-      body: ListView(
-        controller: controller,
-        padding: const EdgeInsets.all(16),
-        children: [
-          Container(height: 100, color: Colors.orange),
-          Container(height: 300, color: Colors.blue),
-          Container(height: 400, color: Colors.grey),
-          Container(height: 200, color: Colors.green),
-          Container(height: 500, color: Colors.amber),
-          Container(height: 700, color: Colors.teal),
         ],
       ),
     );
