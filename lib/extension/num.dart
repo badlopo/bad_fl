214ede58ea1eefbe586300dcd9862bad3f2b8a6a
lopo <lopo@zju.edu.cn>
2024-06-12 15:46:24 +0800

    fix: NumExt -- clamp suffix '0'

--- lib/extension/num.dart
+++ lib/extension/num.dart
@@ -2,16 +2,29 @@ extension NumExt on num {
   String readableFixed([int fractionDigits = 1]) {
     final absVal = abs();
 
+    String raw = '';
+    String unit = '';
+
     switch (absVal) {
       case < 1e3:
-        return toStringAsFixed(fractionDigits);
+        raw = toStringAsFixed(fractionDigits);
+        break;
       case < 1e6:
-        return '${(this / 1e3).toStringAsFixed(fractionDigits)}K';
+        raw = (this / 1e3).toStringAsFixed(fractionDigits);
+        unit = 'K';
+        break;
       case < 1e9:
-        return '${(this / 1e6).toStringAsFixed(fractionDigits)}M';
+        raw = (this / 1e6).toStringAsFixed(fractionDigits);
+        unit = 'M';
+        break;
       default:
-        return '${(this / 1e9).toStringAsFixed(fractionDigits)}B';
+        raw = (this / 1e9).toStringAsFixed(fractionDigits);
+        unit = 'B';
+        break;
     }
+
+    final display = raw.replaceAll(RegExp(r'\.?0+$'), '');
+    return '$display$unit';
   }
 
   String get readable => readableFixed();
