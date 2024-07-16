43516f0ab86ee40c1407e7086064c6090c4f5d7e
lopo <lopo@zju.edu.cn>
2024-07-16 14:00:53 +0800

    feat: BadExpandable -- add BadExpandableSimple with internal maintained state

--- lib/prefab/expandable.dart
+++ lib/prefab/expandable.dart
@@ -69,3 +69,85 @@ class BadExpandable extends StatelessWidget {
     );
   }
 }
+
+/// maintain the open state of the panel internally, expose a [onOpenChanged] callback when the open state changed
+class BadExpandableSimple extends StatefulWidget {
+  /// whether the panel is open initially
+  final bool open;
+
+  /// callback when the open state changed
+  final ValueChanged? onOpenChanged;
+
+  /// gap between the header and the child
+  ///
+  /// Default to `0`
+  final double gap;
+
+  /// builder for the header widget
+  final Widget Function(BadExpandableState state) headerBuilder;
+
+  /// child widget to be displayed when the panel is open
+  ///
+  /// Note: if [child] is `null`, this widget will be treated as an empty panel.
+  final Widget? child;
+
+  const BadExpandableSimple({
+    super.key,
+    this.open = true,
+    this.onOpenChanged,
+    this.gap = 0,
+    required this.headerBuilder,
+    required this.child,
+  });
+
+  @override
+  State<BadExpandableSimple> createState() => _BadExpandableSimpleState();
+}
+
+class _BadExpandableSimpleState extends State<BadExpandableSimple> {
+  late final bool _empty;
+  late bool open;
+
+  @override
+  void initState() {
+    super.initState();
+    _empty = widget.child == null;
+    open = widget.open;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final header = widget.headerBuilder(
+      _empty
+          ? BadExpandableState.empty
+          : open
+              ? BadExpandableState.open
+              : BadExpandableState.close,
+    );
+
+    if (_empty) return header;
+
+    return AnimatedSize(
+      duration: const Duration(milliseconds: 300),
+      onEnd: () => widget.onOpenChanged?.call(open),
+      alignment: Alignment.topCenter,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          BadClickable(
+            onClick: () => setState(() => open = !open),
+            child: header,
+          ),
+          if (open)
+            widget.gap > 0
+                ? Padding(
+                    padding: EdgeInsets.only(top: widget.gap),
+                    child: widget.child!,
+                  )
+                : widget.child!,
+        ],
+      ),
+    );
+  }
+}
