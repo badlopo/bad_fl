d52a524be03e8d2bdb279bc327dbdfce2a14d600
lopo <lopo@zju.edu.cn>
2024-08-02 16:21:48 +0800

    feat: extension/iterable -- enumerate

--- lib/extension/src/iterable.dart
+++ lib/extension/src/iterable.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/widgets.dart';
 
-extension IterableExt<Item> on Iterable<Item> {
+extension IterableSlotted<Item> on Iterable<Item> {
+  /// build a new `List<T>` from each element of the iterable and insert slot elements between every two elements.
   List<Product> slotted<Product>({
     required Product Function(Item item) builder,
     required Product slot,
@@ -21,7 +22,8 @@ extension IterableExt<Item> on Iterable<Item> {
 
 Widget asIs(Widget it) => it;
 
-extension IterableWidgetExt on Iterable<Widget> {
+extension IterableWidgetSlotted on Iterable<Widget> {
+  /// build a new `List<Widget>` from each element of the iterable and insert slot elements between every two elements.
   List<Widget> slotted({
     Widget Function(Widget item) builder = asIs,
     required Widget slot,
@@ -37,5 +39,15 @@ extension IterableWidgetExt on Iterable<Widget> {
       result.add(builder(item));
     }
     return result;
+  }
+}
+
+extension IterableEnumrate<T> on Iterable<T> {
+  /// return a new iterator that yields the current index and the current element of the iterable.
+  ///
+  /// something like https://doc.rust-lang.org/std/iter/struct.Enumerate.html
+  Iterable<(int, T)> get enumerate {
+    int index = 0;
+    return map((item) => (index++, item));
   }
 }
