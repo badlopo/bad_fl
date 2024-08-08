7df9fe3bbead3f8a1d86aa001f176a3e7124c764
lopo <lopo@zju.edu.cn>
2024-08-08 16:31:07 +0800

    refactor: mark deprecated

--- lib/bad_fl.dart
+++ lib/bad_fl.dart
@@ -3,6 +3,7 @@ library bad_fl;
 export 'extension/extensions.dart';
 export 'helper/helper.dart';
 export 'impl/impl.dart';
-export 'mixin/mixin.dart';
 export 'prefab/prefab.dart';
-export 'search_kit/search_kit.dart';
+
+// deprecated stuff
+export 'deprecated/deprecated.dart';
