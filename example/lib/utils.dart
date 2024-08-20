45fca96cfb5d5a2a65ae36b5346f8662681527a3
lopo <lopo@zju.edu.cn>
2024-08-20 22:30:46 +0800

    feat: utils

---
+++ example/lib/utils.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+abstract class Utils {
+  static void showSnackBar(BuildContext context, String message) {
+    ScaffoldMessenger.of(context)
+      ..clearSnackBars()
+      ..showSnackBar(SnackBar(content: Text(message)));
+  }
+}
