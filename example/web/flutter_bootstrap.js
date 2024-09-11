029799bd82f129f32b27bf3152ba6457097d5ff2
lopo <lopo@zju.edu.cn>
2024-09-11 14:19:25 +0800

    chore: references

--- example/web/flutter_bootstrap.js
+++ example/web/flutter_bootstrap.js
@@ -11,3 +11,8 @@ _flutter.loader.load({
         // Make this `app` object available to your JS app.
     }
 });
+
+// https://docs.flutter.cn/platform-integration/web/embedding-flutter-web/
+// https://docs.flutter.dev/platform-integration/web/embedding-flutter-web#embedded-mode
+// https://docs.flutter.dev/platform-integration/web/initialization
+// https://docs.flutter.cn/platform-integration/web/initialization
