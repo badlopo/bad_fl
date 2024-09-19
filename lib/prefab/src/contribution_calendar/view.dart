42ba4400180ecbee406dafea9902dacea61b3e5d
lopo <lopo@zju.edu.cn>
2024-09-19 23:41:10 +0800

    todo: marker

--- lib/prefab/src/contribution_calendar/view.dart
+++ lib/prefab/src/contribution_calendar/view.dart
@@ -1,5 +1,22 @@
 part of 'contribution_calendar.dart';
 
+/// factory function to create a single cell of the calendar
+Widget _createCalendarCell({
+  required double size,
+  required Color color,
+  required BorderRadius? borderRadius,
+}) {
+  return Container(
+    width: size,
+    height: size,
+    decoration: BoxDecoration(color: color, borderRadius: borderRadius),
+  );
+}
+
+// TODO: Mapping: (r, c) => date
+// TODO: calendar column factory
+// TODO: date range (from, to)
+
 class ContributionCalendar extends StatefulWidget {
   final ContributionCalendarController? controller;
   final ContributionCalendarConfig config;
@@ -15,6 +32,17 @@ class ContributionCalendar extends StatefulWidget {
 }
 
 class _ContributionCalendarState extends State<ContributionCalendar> {
+  late final BorderRadius? borderRadius;
+
+  @override
+  void initState() {
+    super.initState();
+
+    borderRadius = widget.config.borderRadius == 0
+        ? null
+        : BorderRadius.circular(widget.config.borderRadius);
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
