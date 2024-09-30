f97a8ad7c0f09ca9dabe5fbdb57ea5ca5d6e3a2c
lopo <lopo@zju.edu.cn>
2024-09-30 10:30:12 +0800

    refactor: mark legacy

--- lib/prefab/src/contribution_calendar/contribution_calendar.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-part 'config.dart';
-
-part 'controller.dart';
-
-part 'view.dart';
---
+++ lib/prefab/src/contribution_calendar_legacy/contribution_calendar.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+part 'config.dart';
+
+part 'controller.dart';
+
+part 'view.dart';
