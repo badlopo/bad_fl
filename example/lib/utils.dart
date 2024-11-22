76ca35ed4e7d0d939a7660b91efa065ac088e75f
lopo <lopo@zju.edu.cn>
2024-11-22 21:28:02 +0800

    refactor: re-init example

--- example/lib/utils.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-abstract class Utils {
-  static void showSnackBar(BuildContext context, String message) {
-    ScaffoldMessenger.of(context)
-      ..clearSnackBars()
-      ..showSnackBar(SnackBar(content: Text(message)));
-  }
-}
