65a75c0c46f72f9f8d947d70191bac83ff08a744
lopo <lopo@zju.edu.cn>
2024-12-23 16:32:25 +0800

    refactor: drop

--- lib/prefab/prefab.dart
+++ lib/prefab/prefab.dart
@@ -1,13 +1,4 @@
-export 'src/contribution_calendar/contribution_calendar.dart';
 export 'src/tree/tree.dart';
 
-export 'src/back_to_top.dart';
-export 'src/expandable.dart';
-export 'src/floating.dart';
-export 'src/panel.dart';
-export 'src/previewer.dart';
-export '../widget/src/refreshable.dart';
-export 'src/scroll_anchor.dart';
-export 'src/swipe_dismiss.dart';
 export 'src/text_field.dart';
 export 'src/webview.dart';
