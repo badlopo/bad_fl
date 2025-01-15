d49d927f93cfca6136250edfb493ca2f6fb9e7d2
lopo <lopo@zju.edu.cn>
2025-01-15 19:46:56 +0800

    refactor: re-implement Result

--- lib/util/src/result.dart
+++ lib/util/src/result.dart
@@ -1,41 +1,77 @@
-/// refer to https://doc.rust-lang.org/std/result/
-sealed class Result<T, E> {
-  const Result();
+// /// refer to https://doc.rust-lang.org/std/result/
+// sealed class Result<T, E> {
+//   const Result();
+//
+//   factory Result.ok(T data) => Ok<T>(data) as Result<T, E>;
+//
+//   factory Result.err(E error) => Err<E>(error) as Result<T, E>;
+//
+//   bool isOk() => this is Ok<T>;
+//
+//   bool isErr() => this is Err<T>;
+//
+//   T unwrap() {
+//     return switch (this) {
+//       Ok<dynamic>() => (this as Ok<T>).data,
+//       Err<dynamic>() => throw StateError('cannot apply unwrap on Err<$E>'),
+//     };
+//   }
+// }
+//
+// class Ok<T> extends Result<T, Never> {
+//   final T data;
+//
+//   const Ok(this.data);
+//
+//   @override
+//   String toString() {
+//     return data.toString();
+//   }
+// }
+//
+// class Err<E> extends Result<Never, E> {
+//   final E error;
+//
+//   const Err(this.error);
+//
+//   @override
+//   String toString() {
+//     return error.toString();
+//   }
+// }
 
-  factory Result.ok(T data) => Ok<T>(data) as Result<T, E>;
+/// Since Dart's inference of generics is very weak, this is the only implementation possible here.
+class Result<T, E> {
+  final bool isOk;
 
-  factory Result.err(E error) => Err<E>(error) as Result<T, E>;
+  bool get isErr => !isOk;
 
-  bool isOk() => this is Ok<T>;
+  final T? _data;
 
-  bool isErr() => this is Err<T>;
-
-  T unwrap() {
-    return switch (this) {
-      Ok<dynamic>() => (this as Ok<T>).data,
-      Err<dynamic>() => throw StateError('cannot apply unwrap on Err<$E>'),
-    };
+  T get data {
+    assert(isOk);
+    return _data!;
   }
-}
 
-class Ok<T> extends Result<T, Never> {
-  final T data;
+  final E? _error;
 
-  const Ok(this.data);
+  E get error {
+    assert(!isOk);
+    return _error!;
+  }
+
+  const Result.ok(T data)
+      : isOk = true,
+        _data = data,
+        _error = null;
+
+  const Result.err(E error)
+      : isOk = true,
+        _data = null,
+        _error = error;
 
   @override
   String toString() {
-    return data.toString();
-  }
-}
-
-class Err<E> extends Result<Never, E> {
-  final E error;
-
-  const Err(this.error);
-
-  @override
-  String toString() {
-    return error.toString();
+    return isOk ? 'Ok($_data)' : 'Err($_error)';
   }
 }
