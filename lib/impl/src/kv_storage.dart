fd8e25f6b4e672d81e6b68e8429f3f43d40326ba
lopo <lopo@zju.edu.cn>
2025-02-14 10:34:12 +0800

    refactor: re-implement 'prelude' & add 'clear'

--- lib/impl/src/kv_storage.dart
+++ lib/impl/src/kv_storage.dart
@@ -1,28 +1,37 @@
+import 'package:bad_fl/core.dart';
 import 'package:hive_ce_flutter/adapters.dart';
 
 /// Key-Value storage implementation.
 abstract class KVStorageImpl {
   /// Hive box instance.
-  static late final Box _box;
+  static Box? _box;
 
-  static Iterable<String> get keys => _box.keys.cast<String>();
+  static Iterable<String> get keys {
+    assert(_box != null, 'call "prelude" first');
+    return _box!.keys.cast<String>();
+  }
 
-  static Map<String, dynamic> get entries =>
-      Map<String, dynamic>.from(_box.toMap());
+  static Map<String, dynamic> get entries {
+    assert(_box != null, 'call "prelude" first');
+    return Map<String, dynamic>.from(_box!.toMap());
+  }
 
   /// Get value by key.
   ///
   /// Return `fallback` if not found, or `null` if `fallback` is not provided.
   static T? get<T>(String key, [T? fallback]) {
-    return _box.get(key, defaultValue: fallback) as T?;
+    assert(_box != null, 'call "prelude" first');
+    return _box!.get(key, defaultValue: fallback) as T?;
   }
 
   static Future<void> set(String key, dynamic value) {
-    return _box.put(key, value);
+    assert(_box != null, 'call "prelude" first');
+    return _box!.put(key, value);
   }
 
   static Future<void> delete(String key) {
-    return _box.delete(key);
+    assert(_box != null, 'call "prelude" first');
+    return _box!.delete(key);
   }
 
   /// Apply patch to storage.
@@ -32,15 +41,32 @@ abstract class KVStorageImpl {
     Iterable<String>? itemsToDelete,
     Map<String, dynamic>? itemsToSet,
   }) async {
+    assert(_box != null, 'call "prelude" first');
     assert(itemsToSet != null || itemsToDelete != null, 'Nothing to patch.');
 
-    if (itemsToDelete != null) await _box.deleteAll(itemsToDelete);
-    if (itemsToSet != null) await _box.putAll(itemsToSet);
+    if (itemsToDelete != null) await _box!.deleteAll(itemsToDelete);
+    if (itemsToSet != null) await _box!.putAll(itemsToSet);
   }
 
-  /// Do prelude before available.
-  static Future<void> prelude(String directory, String boxName) async {
-    await Hive.initFlutter(directory);
-    _box = await Hive.openBox(boxName);
+  /// Removes all entries in the storage.
+  static Future<void> clear() async {
+    assert(_box != null, 'call "prelude" first');
+    await _box!.clear();
+  }
+
+  /// Initialization.
+  static Future<void> prelude() async {
+    if (_box != null) {
+      BadFl.log(
+        module: 'impl/KVStorageImpl',
+        message:
+            'The call to "prelude" is ignored cause it has already been initialized',
+      );
+      return;
+    }
+
+    await Hive.initFlutter('kv_storage');
+    _box = await Hive.openBox('@bad_fl');
+    BadFl.log(module: 'impl/KVStorageImpl', message: 'Initialized');
   }
 }
